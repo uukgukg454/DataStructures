5a391d272323fb9be35f1f711fb8bcaae06b798a
ugupta <ujjwal.gupta@verizonmedia.com>
2022-02-25 01:59:30 +0530

    water trapping and max robbery

--- src/datastructure/array/WaterTrappingProblem.java
+++ src/datastructure/array/WaterTrappingProblem.java
@@ -1,4 +1,65 @@
 package datastructure.array;
 
 public class WaterTrappingProblem {
+    public static void main(String args []) {
+        int [] arr = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+        System.out.println("total water trapped:"+totalWaterStoredWthTwoArrays(arr));
+        System.out.println("total water trapped:"+totalWaterStoredWthNoExtraSpace(arr));
+    }
+
+    static int totalWaterStoredWthTwoArrays(int [] arr) {
+        int result= 0;
+        int n= arr.length;
+        int [] left = new int [n];
+        int [] right = new int [n];
+        left[0]= 0;
+        right[n-1]= 0;
+        int leftMax =arr[0];
+        for(int i=1;i<n;i++) {
+            left[i] = leftMax;
+            if(arr[i]>leftMax) {
+                leftMax = arr[i];
+            }
+        }
+        int rightMax = arr[n-1];
+        for(int i=n-2;i>=0;i--) {
+            right[i] = rightMax;
+            if(arr[i]> rightMax) {
+                rightMax = arr[i];
+            }
+        }
+        for(int i=1;i<n-1;i++) {
+            int maxBoundary = Math.min(left[i],right[i]);
+            if(maxBoundary-arr[i] >0)
+                result = result + maxBoundary-arr[i];
+        }
+        return result;
+    }
+
+    static int totalWaterStoredWthNoExtraSpace(int [] arr) {
+        int n =arr.length;
+        int result =0;
+        int low= 0;
+        int high= n-1;
+        int leftMax= 0;
+        int rightMax =0;
+        while(low<=high) {
+            if(arr[low]<arr[high]) {
+                if(arr[low]>leftMax) {
+                    leftMax = arr[low];
+                } else {
+                    result= result+(leftMax-arr[low]);
+                }
+                low++;
+            } else {
+                if(arr[high]>rightMax) {
+                    rightMax = arr[high];
+                } else {
+                    result = result+(rightMax-arr[high]);
+                }
+                high--;
+            }
+        }
+        return result;
+    }
 }
--- src/datastructure/dp/MaxRobbery.java
+++ src/datastructure/dp/MaxRobbery.java
@@ -1,4 +1,33 @@
 package datastructure.dp;
 
+/**
+ * There are n houses build in a line, each of which contains some value in it. 
+ * A thief is going to steal the maximal value of these houses, 
+ * but he can’t steal in two adjacent houses 
+ * because the owner of the stolen houses will tell his two neighbors left and right side. 
+ * What is the maximum stolen value
+ */
 public class MaxRobbery {
+    public static void main(String []args) {
+        int[] arr = {6, 7, 1, 3, 8, 2, 4};
+        System.out.println("max robbery::"+maxRobbery(arr));
+    }
+
+    static int maxRobbery(int [] arr) {
+        int n = arr.length;
+        if(n==1)
+            return arr[0];
+        else if(n==2)
+            return Math.max(arr[0],arr[1]);
+        else {
+            int [] maxStolenValueArr = new int [n];
+            maxStolenValueArr[0] = arr[0];
+            maxStolenValueArr[1] = Math.max(arr[0],arr[1]);
+            for(int i=2;i<n;i++) {
+                maxStolenValueArr[i]= Math.max(maxStolenValueArr[i-1], (arr[i]+maxStolenValueArr[i-2]));
+            }
+            return maxStolenValueArr[n-1];
+        }
+
+    }
 }
